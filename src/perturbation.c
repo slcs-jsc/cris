add3199b30bcfcc393f0015fa357381a7eb48c42
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2024-02-11 15:37:42 +0100

    Revised perturbation tool to skip scan edges.

--- src/perturbation.c
+++ src/perturbation.c
@@ -4,20 +4,30 @@
    Constants...
    ------------------------------------------------------------ */
 
-/* Number of 4.3 micron channels: */
+/*! Number of 4.3 micron channels: */
 #define N4 54
 
-/* Number of 15 micron low channels: */
+/*! Number of 15 micron low channels: */
 #define N15_LOW 15
 
-/* Number of 15 micron high channels: */
+/*! Number of 15 micron high channels: */
 #define N15_HIGH 1
 
-/* Number of 8.1 micron channels: */
+/*! Number of 8.1 micron channels: */
 #define N8 1
 
-/* Number of 10.4 micron channels: */
+/*! Number of 10.4 micron channels: */
 #define N10 1
+
+/* ------------------------------------------------------------
+   Macros...
+   ------------------------------------------------------------ */
+
+/*! Loop over all footprints. */
+#define LOOP_ALL(ntrack)				\
+  for (track = 0; track < ntrack; track++)		\
+    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)	\
+      for (ifov = 0; ifov < L1_NFOV; ifov++)
 
 /* ------------------------------------------------------------
    Main...
@@ -155,89 +165,77 @@ int main(
     pert_4mu->nfov = L1_NFOV;
     if (pert_4mu->nfov > PERT_NFOV)
       ERRMSG("Too many field of views!");
-    for (track = 0; track < L1_NTRACK; track++)
-      for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-	for (ifov = 0; ifov < L1_NFOV; ifov++) {
-	  pert_4mu->time[track0 + track][xtrack][ifov]
-	    = l1.time[track][xtrack] - 220838400.;
-	  pert_4mu->lon[track0 + track][xtrack][ifov]
-	    = l1.lon[track][xtrack][ifov];
-	  pert_4mu->lat[track0 + track][xtrack][ifov]
-	    = l1.lat[track][xtrack][ifov];
-	}
+    LOOP_ALL(L1_NTRACK) {
+      pert_4mu->time[track0 + track][xtrack][ifov]
+	= l1.time[track][xtrack] - 220838400.;
+      pert_4mu->lon[track0 + track][xtrack][ifov]
+	= l1.lon[track][xtrack][ifov];
+      pert_4mu->lat[track0 + track][xtrack][ifov]
+	= l1.lat[track][xtrack][ifov];
+    }
 
     /* Get 8.1 micron brightness temperature... */
-    for (track = 0; track < L1_NTRACK; track++)
-      for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-	for (ifov = 0; ifov < L1_NFOV; ifov++)
-	  pert_4mu->dc[track0 + track][xtrack][ifov]
-	    = brightness(l1.rad_mw[track][xtrack][ifov][list_8mu[0]] * 1e-3,
-			 l1.nu_mw[list_8mu[0]]);
+    LOOP_ALL(L1_NTRACK)
+      pert_4mu->dc[track0 + track][xtrack][ifov]
+      = brightness(l1.rad_mw[track][xtrack][ifov][list_8mu[0]] * 1e-3,
+		   l1.nu_mw[list_8mu[0]]);
 
     /* Get 10.4 micron brightness temperature... */
-    for (track = 0; track < L1_NTRACK; track++)
-      for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-	for (ifov = 0; ifov < L1_NFOV; ifov++)
-	  pert_15mu_high->dc[track0 + track][xtrack][ifov]
-	    = brightness(l1.rad_lw[track][xtrack][ifov][list_10mu[0]] * 1e-3,
-			 l1.nu_lw[list_10mu[0]]);
+    LOOP_ALL(L1_NTRACK)
+      pert_15mu_high->dc[track0 + track][xtrack][ifov]
+      = brightness(l1.rad_lw[track][xtrack][ifov][list_10mu[0]] * 1e-3,
+		   l1.nu_lw[list_10mu[0]]);
 
     /* Get 4.3 micron brightness temperature... */
-    for (track = 0; track < L1_NTRACK; track++)
-      for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-	for (ifov = 0; ifov < L1_NFOV; ifov++) {
-	  n = 0;
-	  nu = rad = 0;
-	  for (i = 0; i < N4; i++)
-	    if (gsl_finite(l1.rad_sw[track][xtrack][ifov][list_4mu[i]])) {
-	      rad += l1.rad_sw[track][xtrack][ifov][list_4mu[i]] * 1e-3;
-	      nu += l1.nu_sw[list_4mu[i]];
-	      n++;
-	    }
-	  if (n > 0.9 * N4)
-	    pert_4mu->bt[track0 + track][xtrack][ifov] =
-	      brightness(rad / n, nu / n);
-	  else
-	    pert_4mu->bt[track0 + track][xtrack][ifov] = GSL_NAN;
+    LOOP_ALL(L1_NTRACK) {
+      n = 0;
+      nu = rad = 0;
+      for (i = 0; i < N4; i++)
+	if (gsl_finite(l1.rad_sw[track][xtrack][ifov][list_4mu[i]])) {
+	  rad += l1.rad_sw[track][xtrack][ifov][list_4mu[i]] * 1e-3;
+	  nu += l1.nu_sw[list_4mu[i]];
+	  n++;
 	}
+      if (n > 0.9 * N4)
+	pert_4mu->bt[track0 + track][xtrack][ifov] =
+	  brightness(rad / n, nu / n);
+      else
+	pert_4mu->bt[track0 + track][xtrack][ifov] = GSL_NAN;
+    }
 
     /* Get 15 micron low brightness temperature... */
-    for (track = 0; track < L1_NTRACK; track++)
-      for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-	for (ifov = 0; ifov < L1_NFOV; ifov++) {
-	  n = 0;
-	  nu = rad = 0;
-	  for (i = 0; i < N15_LOW; i++)
-	    if (gsl_finite(l1.rad_lw[track][xtrack][ifov][list_15mu_low[i]])) {
-	      rad += l1.rad_lw[track][xtrack][ifov][list_15mu_low[i]] * 1e-3;
-	      nu += l1.nu_lw[list_15mu_low[i]];
-	      n++;
-	    }
-	  if (n > 0.9 * N15_LOW)
-	    pert_15mu_low->bt[track0 + track][xtrack][ifov] =
-	      brightness(rad / n, nu / n);
-	  else
-	    pert_15mu_low->bt[track0 + track][xtrack][ifov] = GSL_NAN;
+    LOOP_ALL(L1_NTRACK) {
+      n = 0;
+      nu = rad = 0;
+      for (i = 0; i < N15_LOW; i++)
+	if (gsl_finite(l1.rad_lw[track][xtrack][ifov][list_15mu_low[i]])) {
+	  rad += l1.rad_lw[track][xtrack][ifov][list_15mu_low[i]] * 1e-3;
+	  nu += l1.nu_lw[list_15mu_low[i]];
+	  n++;
 	}
+      if (n > 0.9 * N15_LOW)
+	pert_15mu_low->bt[track0 + track][xtrack][ifov] =
+	  brightness(rad / n, nu / n);
+      else
+	pert_15mu_low->bt[track0 + track][xtrack][ifov] = GSL_NAN;
+    }
 
     /* Get 15 micron high brightness temperature... */
-    for (track = 0; track < L1_NTRACK; track++)
-      for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-	for (ifov = 0; ifov < L1_NFOV; ifov++) {
-	  n = 0;
-	  nu = rad = 0;
-	  for (i = 0; i < N15_HIGH; i++)
-	    if (gsl_finite(l1.rad_lw[track][xtrack][ifov][list_15mu_high[i]])) {
-	      rad += l1.rad_lw[track][xtrack][ifov][list_15mu_high[i]] * 1e-3;
-	      nu += l1.nu_lw[list_15mu_high[i]];
-	      n++;
-	    }
-	  if (n > 0.9 * N15_HIGH)
-	    pert_15mu_high->bt[track0 + track][xtrack][ifov] =
-	      brightness(rad / n, nu / n);
-	  else
-	    pert_15mu_high->bt[track0 + track][xtrack][ifov] = GSL_NAN;
+    LOOP_ALL(L1_NTRACK) {
+      n = 0;
+      nu = rad = 0;
+      for (i = 0; i < N15_HIGH; i++)
+	if (gsl_finite(l1.rad_lw[track][xtrack][ifov][list_15mu_high[i]])) {
+	  rad += l1.rad_lw[track][xtrack][ifov][list_15mu_high[i]] * 1e-3;
+	  nu += l1.nu_lw[list_15mu_high[i]];
+	  n++;
 	}
+      if (n > 0.9 * N15_HIGH)
+	pert_15mu_high->bt[track0 + track][xtrack][ifov] =
+	  brightness(rad / n, nu / n);
+      else
+	pert_15mu_high->bt[track0 + track][xtrack][ifov] = GSL_NAN;
+    }
 
     /* Increment track counter... */
     track0 += L1_NTRACK;
@@ -253,6 +251,18 @@ int main(
   /* Loop over scans and field of views... */
   for (track = 0; track < pert_4mu->ntrack; track++)
     for (ifov = 0; ifov < L1_NFOV; ifov++) {
+
+      /* Skip scan edges... */
+      pert_4mu->dc[track][0][ifov] = GSL_NAN;
+      pert_4mu->dc[track][L1_NXTRACK - 1][ifov] = GSL_NAN;
+      pert_15mu_high->dc[track][0][ifov] = GSL_NAN;
+      pert_15mu_high->dc[track][L1_NXTRACK - 1][ifov] = GSL_NAN;
+      pert_4mu->bt[track][0][ifov] = GSL_NAN;
+      pert_4mu->bt[track][L1_NXTRACK - 1][ifov] = GSL_NAN;
+      pert_15mu_low->bt[track][0][ifov] = GSL_NAN;
+      pert_15mu_low->bt[track][L1_NXTRACK - 1][ifov] = GSL_NAN;
+      pert_15mu_high->bt[track][0][ifov] = GSL_NAN;
+      pert_15mu_high->bt[track][L1_NXTRACK - 1][ifov] = GSL_NAN;
 
       /* Get 4 micron perturbations... */
       for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++) {
@@ -293,76 +303,71 @@ int main(
   LOG(1, "Calculate variances...");
 
   /* Loop over footprints... */
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++) {
+  LOOP_ALL(pert_4mu->ntrack) {
 
-	/* Get geolocation... */
-	geo2cart(0.0, pert_4mu->lon[track][xtrack][ifov],
-		 pert_4mu->lat[track][xtrack][ifov], x0);
+    /* Get geolocation... */
+    geo2cart(0.0, pert_4mu->lon[track][xtrack][ifov],
+	     pert_4mu->lat[track][xtrack][ifov], x0);
 
-	/* Init... */
-	int n_4mu = 0, n_15mu_low = 0, n_15mu_high = 0;
-	double mean_4mu = 0, mean_15mu_low = 0, mean_15mu_high = 0;
-	double var_4mu = 0, var_15mu_low = 0, var_15mu_high = 0;
+    /* Init... */
+    int n_4mu = 0, n_15mu_low = 0, n_15mu_high = 0;
+    double mean_4mu = 0, mean_15mu_low = 0, mean_15mu_high = 0;
+    double var_4mu = 0, var_15mu_low = 0, var_15mu_high = 0;
 
-	/* Loop over neighbouring footprints... */
-	for (track2 = track - dtrack; track2 <= track + dtrack; track2++)
-	  for (xtrack2 = xtrack - dxtrack; xtrack2 <= xtrack + dxtrack;
-	       xtrack2++)
-	    for (ifov2 = 0; ifov2 < L1_NFOV; ifov2++)
-	      if (track2 >= 0 && track2 < pert_4mu->ntrack
-		  && xtrack2 >= 0 && xtrack2 < L1_NXTRACK) {
+    /* Loop over neighbouring footprints... */
+    for (track2 = track - dtrack; track2 <= track + dtrack; track2++)
+      for (xtrack2 = xtrack - dxtrack; xtrack2 <= xtrack + dxtrack; xtrack2++)
+	for (ifov2 = 0; ifov2 < L1_NFOV; ifov2++)
+	  if (track2 >= 0 && track2 < pert_4mu->ntrack
+	      && xtrack2 >= 0 && xtrack2 < L1_NXTRACK) {
 
-		/* Check horizontal distance... */
-		geo2cart(0.0, pert_4mu->lon[track2][xtrack2][ifov2],
-			 pert_4mu->lat[track2][xtrack2][ifov2], x1);
-		if (DIST2(x0, x1) > var_dh2)
-		  continue;
+	    /* Check horizontal distance... */
+	    geo2cart(0.0, pert_4mu->lon[track2][xtrack2][ifov2],
+		     pert_4mu->lat[track2][xtrack2][ifov2], x1);
+	    if (DIST2(x0, x1) > var_dh2)
+	      continue;
 
-		/* Calculate variance... */
-		if (gsl_finite(pert_4mu->pt[track2][xtrack2][ifov2])) {
-		  mean_4mu += pert_4mu->pt[track2][xtrack2][ifov2];
-		  var_4mu += gsl_pow_2(pert_4mu->pt[track2][xtrack2][ifov2]);
-		  n_4mu++;
-		}
+	    /* Calculate variance... */
+	    if (gsl_finite(pert_4mu->pt[track2][xtrack2][ifov2])) {
+	      mean_4mu += pert_4mu->pt[track2][xtrack2][ifov2];
+	      var_4mu += gsl_pow_2(pert_4mu->pt[track2][xtrack2][ifov2]);
+	      n_4mu++;
+	    }
 
-		if (gsl_finite(pert_15mu_low->pt[track2][xtrack2][ifov2])) {
-		  mean_15mu_low += pert_15mu_low->pt[track2][xtrack2][ifov2];
-		  var_15mu_low +=
-		    gsl_pow_2(pert_15mu_low->pt[track2][xtrack2][ifov2]);
-		  n_15mu_low++;
-		}
+	    if (gsl_finite(pert_15mu_low->pt[track2][xtrack2][ifov2])) {
+	      mean_15mu_low += pert_15mu_low->pt[track2][xtrack2][ifov2];
+	      var_15mu_low +=
+		gsl_pow_2(pert_15mu_low->pt[track2][xtrack2][ifov2]);
+	      n_15mu_low++;
+	    }
 
-		if (gsl_finite(pert_15mu_high->pt[track2][xtrack2][ifov2])) {
-		  mean_15mu_high +=
-		    pert_15mu_high->pt[track2][xtrack2][ifov2];
-		  var_15mu_high +=
-		    gsl_pow_2(pert_15mu_high->pt[track2][xtrack2][ifov2]);
-		  n_15mu_high++;
-		}
-	      }
+	    if (gsl_finite(pert_15mu_high->pt[track2][xtrack2][ifov2])) {
+	      mean_15mu_high += pert_15mu_high->pt[track2][xtrack2][ifov2];
+	      var_15mu_high +=
+		gsl_pow_2(pert_15mu_high->pt[track2][xtrack2][ifov2]);
+	      n_15mu_high++;
+	    }
+	  }
 
-	/* Save variance data... */
-	if (n_4mu > 0)
-	  pert_4mu->var[track][xtrack][ifov] =
-	    var_4mu / n_4mu - gsl_pow_2(mean_4mu / n_4mu);
-	else
-	  pert_4mu->var[track][xtrack][ifov] = GSL_NAN;
+    /* Save variance data... */
+    if (n_4mu > 0 && xtrack > 0 && xtrack < L1_NXTRACK - 1)
+      pert_4mu->var[track][xtrack][ifov] =
+	var_4mu / n_4mu - gsl_pow_2(mean_4mu / n_4mu);
+    else
+      pert_4mu->var[track][xtrack][ifov] = GSL_NAN;
 
-	if (n_15mu_low > 0)
-	  pert_15mu_low->var[track][xtrack][ifov] =
-	    var_15mu_low / n_15mu_low - gsl_pow_2(mean_15mu_low / n_15mu_low);
-	else
-	  pert_15mu_low->var[track][xtrack][ifov] = GSL_NAN;
+    if (n_15mu_low > 0 && xtrack > 0 && xtrack < L1_NXTRACK - 1)
+      pert_15mu_low->var[track][xtrack][ifov] =
+	var_15mu_low / n_15mu_low - gsl_pow_2(mean_15mu_low / n_15mu_low);
+    else
+      pert_15mu_low->var[track][xtrack][ifov] = GSL_NAN;
 
-	if (n_15mu_high > 0)
-	  pert_15mu_high->var[track][xtrack][ifov] =
-	    var_15mu_high / n_15mu_high -
-	    gsl_pow_2(mean_15mu_high / n_15mu_high);
-	else
-	  pert_15mu_high->var[track][xtrack][ifov] = GSL_NAN;
-      }
+    if (n_15mu_high > 0 && xtrack > 0 && xtrack < L1_NXTRACK - 1)
+      pert_15mu_high->var[track][xtrack][ifov] =
+	var_15mu_high / n_15mu_high - gsl_pow_2(mean_15mu_high / n_15mu_high);
+    else
+      pert_15mu_high->var[track][xtrack][ifov] = GSL_NAN;
+  }
 
   /* ------------------------------------------------------------
      Write netCDF file...
@@ -434,101 +439,73 @@ int main(
 
   /* Write data... */
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->time[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->time[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, time_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->lon[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->lon[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, lon_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->lat[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->lat[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, lat_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->dc[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->dc[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_8mu_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_high->dc[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_high->dc[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_10mu_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->bt[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->bt[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_4mu_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->pt[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->pt[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_4mu_pt_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_4mu->var[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_4mu->var[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_4mu_var_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_low->bt[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_low->bt[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_15mu_low_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_low->pt[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_low->pt[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_15mu_low_pt_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_low->var[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_low->var[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_15mu_low_var_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_high->bt[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_high->bt[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_15mu_high_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_high->pt[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_high->pt[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_15mu_high_pt_varid, help));
 
   n = 0;
-  for (track = 0; track < pert_4mu->ntrack; track++)
-    for (xtrack = 0; xtrack < L1_NXTRACK; xtrack++)
-      for (ifov = 0; ifov < L1_NFOV; ifov++)
-	help[n++] = pert_15mu_high->var[track][xtrack][ifov];
+  LOOP_ALL(pert_4mu->ntrack)
+    help[n++] = pert_15mu_high->var[track][xtrack][ifov];
   NC(nc_put_var_double(ncid, bt_15mu_high_var_varid, help));
 
   /* Close file... */
