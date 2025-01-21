c435812e6e98549f37ac5fe9193224b54ca80618
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-01-21 15:28:41 +0100

    Update of libraries.

--- libs/build.sh
+++ libs/build.sh
@@ -1,40 +1,47 @@
 #! /bin/bash
 
 # Setup...
-#rm -rf build
+rm -rf build
 target=$(mkdir -p build && cd build && pwd)
 
 # Prepare directories...
 mkdir -p $target/src $target/bin $target/lib $target/man/man1 \
-    && cp *tar.gz $target/src \
+    && cp *tar.bz2 $target/src \
     && cd $target/src \
-    && for f in $(ls *tar.gz) ; do tar xvzf $f ; done \
+    && for f in $(ls *tar.bz2) ; do tar xvjf $f ; done \
     || exit
 
 # GSL...
-dir=gsl-2.7
+dir=gsl-2.7.1
 cd $target/src/$dir \
     && ./configure --prefix=$target \
-    && make && make check && make install && make clean \
+    && make -j && make check && make install && make clean \
 	|| exit
 
 # zlib...
-dir=zlib-1.2.12
+dir=zlib-1.3.1
 cd $target/src/$dir \
     && ./configure --prefix=$target \
-    && make && make check && make install && make clean \
+    && make -j && make check && make install && make clean \
+	|| exit
+
+# szip...
+dir=szip-2.1.1
+cd $target/src/$dir \
+    && ./configure --prefix=$target \
+    && make -j && make check && make install && make clean \
 	|| exit
 
 # HDF5...
-dir=hdf5-1.12.1
+dir=hdf5-1.14.4-3
 cd $target/src/$dir \
-    && ./configure --prefix=$target --with-zlib=$target --enable-hl \
-    && make && make check && make install && make clean \
+    && ./configure --prefix=$target --with-zlib=$target --with-szlib=$target --enable-hl --disable-fortran \
+    && make -j && make check && make install && make clean \
 	|| exit
 
 # netCDF...
-dir=netcdf-c-4.8.1
+dir=netcdf-c-4.9.2
 cd $target/src/$dir \
-    && CPPFLAGS=-I$target/include LDFLAGS=-L$target/lib ./configure --prefix=$target --disable-dap --disable-nczarr \
-    && make && make install && make clean \
+    && CPPFLAGS=-I$target/include LDFLAGS=-L$target/lib ./configure --prefix=$target --disable-dap --disable-byterange --disable-nczarr --disable-libxml2 \
+    && make -j && make install && make clean \
 	|| exit
