4eecd6fc2142f2d2aa4ea2f39ec03ae80c3583c8
Lars Hoffmann <l.hoffmann@fz-juelich.de>
2025-02-05 20:24:26 +0100

    Added test of volcano data.

---
+++ tests/volc_test/run.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Set environment...
+export LD_LIBRARY_PATH=../../libs/build/lib:$LD_LIBRARY_PATH
+export OMP_NUM_THREADS=4
+
+# Setup...
+cris=../../src
+
+# Uncompress test data...
+cd ../data \
+    && zip -s 0 cris_l1b.zip --out cris_l1b_full.zip \
+    && unzip -o cris_l1b_full.zip \
+    && cd - || exit
+
+# Create directory...
+rm -rf data && mkdir -p data
+
+# Extract volcano data...
+$cris/volcano - data/volcano_noapo.tab ../data/SNDR.SNPP.CRIS.20220115T0000.m06.g001.L1B.std.v03_08.G.220115064421.nc
+$cris/volcano - data/volcano_apo.tab ../data/SNDR.SNPP.CRIS.20220115T0000.m06.g001.L1B.std.v03_08.G.220115064421.nc APO 1
+
+# Compare files...
+echo -e "\nCompare results..."
+error=0
+for f in $(ls data.ref/*.tab) ; do
+    diff -q -s data/"$(basename "$f")" "$f" || error=1
+done
+exit $error
