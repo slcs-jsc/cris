be4e20b1f6c5c231aeca21485361ca4aa71c4a3a
lars2015 <lars2015@users.noreply.github.com>
2025-12-07 18:10:32 +0000

    deploy: 95a0053e1554df6a44c8ae9904faf880dc49d726

---
+++ doxygen/search/all_12.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['sampling_2ec_0',['sampling.c',['../sampling_8c.html',1,'']]],
+  ['sat_5flat_1',['sat_lat',['../structcris__l1__t.html#a97d6bc61eac0186abf57f536f4fecbfc',1,'cris_l1_t']]],
+  ['sat_5flon_2',['sat_lon',['../structcris__l1__t.html#acfa936bd9b789db50f38b0ee237d9a39',1,'cris_l1_t']]],
+  ['sat_5fz_3',['sat_z',['../structcris__l1__t.html#a175cd6e5b49372669ada2217011f3427',1,'cris_l1_t']]],
+  ['scan_5fctl_4',['scan_ctl',['../jurassic_8h.html#abb6ce9b0361afe11cb23b8ac1e0c7fa3',1,'scan_ctl(int argc, char *argv[], const char *varname, const int arridx, const char *defvalue, char *value):&#160;jurassic.c'],['../jurassic_8c.html#abb6ce9b0361afe11cb23b8ac1e0c7fa3',1,'scan_ctl(int argc, char *argv[], const char *varname, const int arridx, const char *defvalue, char *value):&#160;jurassic.c']]],
+  ['set_5fcov_5fapr_5',['set_cov_apr',['../jurassic_8c.html#ae9a11e5f03e0a307add4daf864977cec',1,'set_cov_apr(const ret_t *ret, const ctl_t *ctl, const atm_t *atm, const int *iqa, const int *ipa, gsl_matrix *s_a):&#160;jurassic.c'],['../jurassic_8h.html#ae9a11e5f03e0a307add4daf864977cec',1,'set_cov_apr(const ret_t *ret, const ctl_t *ctl, const atm_t *atm, const int *iqa, const int *ipa, gsl_matrix *s_a):&#160;jurassic.c']]],
+  ['set_5fcov_5fmeas_6',['set_cov_meas',['../jurassic_8h.html#a6bb69eac608c4019c689bf8280912c1f',1,'set_cov_meas(const ret_t *ret, const ctl_t *ctl, const obs_t *obs, gsl_vector *sig_noise, gsl_vector *sig_formod, gsl_vector *sig_eps_inv):&#160;jurassic.c'],['../jurassic_8c.html#a6bb69eac608c4019c689bf8280912c1f',1,'set_cov_meas(const ret_t *ret, const ctl_t *ctl, const obs_t *obs, gsl_vector *sig_noise, gsl_vector *sig_formod, gsl_vector *sig_eps_inv):&#160;jurassic.c']]],
+  ['sfeps_7',['sfeps',['../structlos__t.html#addaf4b2df1ce35bb8e4e43bd6d61e2b2',1,'los_t::sfeps()'],['../structatm__t.html#a09f5c1a2a0ecac7b481f3ea138b8a750',1,'atm_t::sfeps()']]],
+  ['sfnu_8',['sfnu',['../structctl__t.html#a29804fac77623ca3d1e32f92955ac501',1,'ctl_t']]],
+  ['sfsza_9',['sfsza',['../structctl__t.html#a8e7cd04d65ba7ae0efb60e09678a566c',1,'ctl_t']]],
+  ['sft_10',['sft',['../structatm__t.html#a651f52626605a4dd79dd147ee245014f',1,'atm_t::sft()'],['../structlos__t.html#a203d2c0dc9cc278bbd6b51714c759191',1,'los_t::sft()']]],
+  ['sftype_11',['sftype',['../structctl__t.html#a8ff7b3f429eb1f23d768fd189d963169',1,'ctl_t']]],
+  ['size_12',['size',['../structtbl__gas__index__t.html#ab193f1054a9cac976d5ea6d76403df09',1,'tbl_gas_index_t']]],
+  ['spec2tab_2ec_13',['spec2tab.c',['../spec2tab_8c.html',1,'']]],
+  ['sr_14',['sr',['../structtbl__t.html#a0829f62f18df6865078916867fb45049',1,'tbl_t']]],
+  ['src_15',['src',['../structlos__t.html#ab882cd1e0293656de28123cd377d5d7b',1,'los_t']]],
+  ['st_16',['st',['../structtbl__t.html#acbbff4883292be53dfee5ec3258e0bed',1,'tbl_t']]],
+  ['sza_17',['sza',['../jurassic_8h.html#ad1500cc96441cf118acda392366397cc',1,'jurassic.h']]]
+];
---
+++ doxygen/search/defines_b.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['m_0',['M',['../jurassic_8h.html#a52037c938e3c1b126c6277da5ca689d0',1,'jurassic.h']]],
+  ['max_1',['MAX',['../jurassic_8h.html#afa99ec4acc4ecb2dc3c2d05da15d0e3f',1,'jurassic.h']]],
+  ['max_5ftables_2',['MAX_TABLES',['../jurassic_8h.html#a6f4cdd0162e9f2ab251fa5b23410c8da',1,'jurassic.h']]],
+  ['me_3',['ME',['../jurassic_8h.html#adf8d34f3be20164c292bccfeb72898e5',1,'jurassic.h']]],
+  ['min_4',['MIN',['../jurassic_8h.html#a3acffbd305ee72dcd4593c0d8af64a4f',1,'jurassic.h']]]
+];
